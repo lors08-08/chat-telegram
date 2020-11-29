cb3a390e56e2238cfda430d96f0f8259e4ed5355
lors <lors080898@icloud.com>
2020-11-29 22:31:36 +0300

    added git icon

--- src/Components/MainBox/MainBox.js
+++ src/Components/MainBox/MainBox.js
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import Chat from "../../Components/MainBox/Chat";
 import MainBoxBottom from "./MainBoxBottom";
 import MainBoxHeader from "./MainBoxHeader";
+import githubIcon from "./Icons/githubIcon.svg"
 
 function MainBox() {
   const chatSelected = useParams().id;
@@ -11,16 +12,22 @@ function MainBox() {
   const selectedOne = contact.find((el) => {
     return el._id === chatSelected;
   });
-
-  return selectedOne ? (
-    <div className="main-box">
-      <MainBoxHeader />
-      <Chat />
-      <MainBoxBottom />
-    </div>
-  ) : (
-    <div className="box-loading" id="bottom">Please, select a chat to start messaging</div>
-  );
+  return (
+    <>
+      <a href="https://github.com/lors08-08/chat-telegram"><img src={githubIcon} className="github-icon"/></a>
+      {selectedOne ? (
+        <div className="main-box">
+          <MainBoxHeader/>
+          <Chat/>
+          <MainBoxBottom/>
+        </div>
+      ) : (
+        <div className="box-loading" id="bottom">
+          Please, select a chat to start messaging
+        </div>
+      )}
+    </>
+  )
 
 
 }
